21d48ceeae826b954d8de4799463384df6371a96
Osmonkulov Zhyldyzbek <osmonkulov.01@gmail.com>
2022-01-31 20:53:03 +0600

    add text

--- src/kg/megacom/Main.java
+++ src/kg/megacom/Main.java
@@ -10,6 +10,7 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("Добавил код");
         Scanner scanner = new Scanner(System.in);
         WishService wishService = new WishServiceImpl();
         while (true){
